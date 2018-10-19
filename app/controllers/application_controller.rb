b7006d7519e81a6de767718fbea5e6bada62e4af
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-10-19 12:31:01 +0900

    create devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  # before_filter :set_time_zone, if: :user_signed_in?
   protect_from_forgery with: :exception
+
+  # private
+
+  #   def set_time_zone
+  #     Time.zone = current_user.time_zone
+  #   end
+
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,13 @@
 class BlogsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @blogs = Blog.all
+  end
+
+  def new
+  end
+
+  def create
   end
 end
